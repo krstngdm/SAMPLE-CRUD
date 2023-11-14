144cc8465de16d9a79657ea0275b00dee45f190b
Zhieve12 <145948828+krstngdm@users.noreply.github.com>
2023-11-15 00:30:55 +0800

    .

---
+++ forms/addform.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <title>Add Data</title>
+</head>
+<body>
+    <h1>Add subject</h1>
+    <a href="../index.php">Home</a>
+    <br><br/>
+    <form action="../functions/add.php" method="post" name="form1">
+        <table width="25%" border="0">
+            <tr>
+                <td>Subject Code</td>
+                <td><input type="text" name="code" id=""></td>
+            </tr>
+            <tr>
+                <td>Subject Name</td>
+                <td><input type="text" name="name" id=""></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type="submit" name="Submit" value="Add"></td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>

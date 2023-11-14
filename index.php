5d56801d7174852c605fd633a4a140bb5154e1d0
Zhieve12 <145948828+krstngdm@users.noreply.github.com>
2023-11-15 00:05:02 +0800

    Update index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+    include_once("dbconnection/mysqlconfig_connection.php");
+    include_once("functions/fetch.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>SAMPLE CRUD</title>
+</head>
+<body>
+    <h1>My Subject</h1>
+    <a href="forms/addforms.php">Add Subject</a>
+    <table width='100%' border= 1>
+        <tr bgcolor='#CCCCCC'>
+            <td>ID</td>
+            <td>Subject Code</td>
+            <td>Subject Name</td>
+            <td>Action</td>
+        </tr>
+        <?php
+            while($res = mysqli_fetch_array($result)){
+                echo "<tr>";
+                echo "<td>".$res['subject_id']."</td>";
+                echo "<td>".$res['subject_code']."</td>";
+                echo "<td></td><a href=\"forms/editform.php?id=$res[subject_id]\">Edit</a> |
+                        <a href=\"functions/delete.php?id=$res[subject_id]\"
+                        onClick=\"return confirm('Are you sure you want to delete?')\">DELETE</a></td>";
+            }
+        ?>
+    </table>
+</body>
+</html>

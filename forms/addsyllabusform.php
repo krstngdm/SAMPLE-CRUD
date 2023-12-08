9a0e7ba3061e08a474f5e5e3ed1be16957892771
Zhieve12 <145948828+krstngdm@users.noreply.github.com>
2023-12-08 22:51:42 +0800

    Update addsyllabusform.php

--- forms/addsyllabusform.php
+++ forms/addsyllabusform.php
@@ -16,9 +16,16 @@
                     echo "<font color='red'>Syllabus Code field is empty</font><br/>";
                 }
                 if(empty($author)){
-                    echo "<font color='red'> "
+                    echo "<font color='red'>Syllabus Author field is empty</font><br/>";
                 }
+                echo "<br/><a href='javascript:self.history.black()'>Go Back</a>";
+            }
+            else{
+                $_result = mysqli_query($dbc,"<<UPDATE SQL QUERY>>");
+                echo "<font color='green'>Data added successfully.</font>";
+                echo "<br/><a href='../index.php>View Result</a>'";
             }
         }
+        ?>
     </body>
 </html>
